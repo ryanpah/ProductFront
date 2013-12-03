56f2e92acc4865496b984f197006dabecf953b94
Ryan Page <rpage326@gmail.com>
2013-12-03 15:03:19 -0500

    ran devise install g

---
+++ db/migrate/20131203195915_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.text :name
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2
+      t.text :images, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
